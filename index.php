58c784834d9c459bf38a948cb3abacdeef705bd5
datagutt <datagutt@lekanger.no>
2013-03-10 13:33:58 +0100

    Basic hawalius site

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Hawalius
+ *
+ * @copyright Copyright (c) 2013, Thomas lekanger
+ * @version 1.0
+ */
+
+if(!defined('HAWALIUS_PATH')){
+	define('HAWALIUS_PATH', dirname(__FILE__));
+}
+define('ENV', 'development');
+
+require HAWALIUS_PATH . '/app/index.php';
