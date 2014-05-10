0805a03c5c9b99acf37812f96c63db57c6e6a39f
datagutt <datagutt@lekanger.no>
2014-05-10 18:18:13 +0200

    Increase minor version because of huge refactor

--- index.php
+++ index.php
@@ -10,6 +10,6 @@ if(!defined('HAWALIUS_PATH')){
 	define('HAWALIUS_PATH', dirname(__FILE__));
 }
 define('ENV', 'development');
-define('VERSION', '0.1.0');
+define('VERSION', '0.2.0');
 
 require HAWALIUS_PATH . '/app/index.php';
